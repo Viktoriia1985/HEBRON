28bae78e86650ef778b29de9b1260d4fd4538d48
Viktoriia1985 <flightsspb@gmail.com>
2022-04-05 21:34:30 +0300

    lesson 13

--- JS/lesson_13_Recursion2_&_GitHub/lesson_13.js
+++ JS/lesson_13_Recursion2_&_GitHub/lesson_13.js
@@ -1,75 +1,77 @@
 // //     TASK   "Depth"
 
 
-const superDeepObj1 = {
-    one: {
-        two: {
-            three: {
-                four: {
-                    five: {
-                        arr: [],
-                        six: 6
-                    },
-                    hello: 'world',
-                    arr: []
-                }
-            }
-        }
-    }
-};
-
-const superDeepObj2 = {
-    one: {
-        innerOne: {
-            innerTwo: {}
-        }
-    },
-    one2: {
-        two: {
-            three: {
-                four: 10,
-                fourq: [{
-                    user: {
-                        name: 'Vova'
-                    }
-                }]
-            }
-        }
-    }
-};
-
-console.log(Object.values(superDeepObj2));
-
-let deepIndex = -Infinity;
-
-const deepCounter = (object, deepCount = 0) => {
-    if (typeof object !== 'object' && object !== null) {
-        if (deepIndex < deepCount) {
-            deepIndex = deepCount;
-        }
-        return;
-    }
-
-const values = Object.values(object);
-
-    for (const singleObject of values) {
-        deepCounter(singleObject, ++deepCount);
-    }
-};
-
-deepCounter(superDeepObj1);
-console.log(deepIndex);
-
-console.log('********************************');
-
-deepCounter(superDeepObj2);
-console.log(deepIndex);
-
+// const superDeepObj1 = {
+//     one: {
+//         two: {
+//             three: {
+//                 four: {
+//                     five: {
+//                         arr: [],
+//                         six: 6
+//                     },
+//                     hello: 'world',
+//                     arr: []
+//                 }
+//             }
+//         }
+//     }
+// };
+//
+// const superDeepObj2 = {
+//     one: {
+//         innerOne: {
+//             innerTwo: {}
+//         }
+//     },
+//     one2: {
+//         two: {
+//             three: {
+//                 four: 10,
+//                 fourq: [{
+//                     user: {
+//                         name: 'Vova'
+//                     }
+//                 }]
+//             }
+//         }
+//     }
+// };
+//
+// console.log(Object.values(superDeepObj2));
+//
+// let deepIndex = -Infinity;
+//
+// const deepCounter = (object, deepCount = 0) => {
+//     if (typeof object !== 'object' && object !== null) {
+//         if (deepIndex < deepCount) {
+//             deepIndex = deepCount;
+//         }
+//         return;
+//     }
+//
+// const values = Object.values(object);
+//
+//     for (const singleObject of values) {
+//         deepCounter(singleObject, ++deepCount);
+//     }
+// };
+//
+// deepCounter(superDeepObj1);
+// console.log(deepIndex);
+//
+// console.log('********************************');
+//
+// deepCounter(superDeepObj2);
+// console.log(deepIndex);
+//
 
 
 ///////////////////////////////////////////////////////////////////////////
 
-// GIT HUB  - 00: 48 : 11
+// GIT HUB  - 00: 50 : 00
+
+
 
 
 
